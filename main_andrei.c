c7929e8d182a6bd5512b25a0612820736ad4f472
Enshin Andrei <b1os@bk.ru>
2025-01-04 22:54:41 +0900

    play with readline

--- main_andrei.c
+++ main_andrei.c
@@ -1,12 +1,25 @@
-//TODO add function to free context struct on exit
+/*
+TODO
+	add function to free context struct on exit
+*/
+
+/* Q
+	how -l option works in gcc?
+*/
+
 
 /*
-	gcc -Wall -Wextra -Werror main_andrei.c
+	gcc -lreadline -Wall -Wextra -Werror main_andrei.c && ./a.out
 */
 
 #include <stdio.h>
+#include <readline/readline.h>
+
 
 int main(int argc, char *argv[])
 {
+	char *line;
+	line = readline("yo> ");
 	printf("argc [%d], argv addr [%p]\n", argc, argv);
+	printf("line [%s]\n", line);
 }
