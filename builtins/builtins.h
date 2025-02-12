218d16cc0d08c426f9090e235b54a5c0d25d22fe
Adrian gutierrez <19gutierreza@gmail.com>
2025-02-12 23:56:10 +0900

    fixed export.h

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -6,7 +6,7 @@
 /*   By: adrgutie <adrgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 01:27:12 by adrgutie          #+#    #+#             */
-/*   Updated: 2025/02/12 23:20:25 by adrgutie         ###   ########.fr       */
+/*   Updated: 2025/02/12 23:55:26 by adrgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 int	cmd_echo(char **args);
 int	cmd_cd(char *path);
 int	cmd_pwd(void);
-int	cmd_export(char **args, t_variable *vars);
+int	cmd_export(char **args, t_envs *envs);
 int	cmd_unset(char **args, t_variable *vars);
 int	cmd_env(t_envs *envs);
 int	cmd_exit(char **args, t_minishell *ms, t_pipex *spipex);
