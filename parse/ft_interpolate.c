3f55a693b7717cec1b4160fd6351c69cd6cb9359
Adrian Gutierrez Valero <adrgutie@c5r3s10.42tokyo.jp>
2025-03-15 22:08:01 +0900

    started interpolation@

---
+++ parse/ft_interpolate.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_interpolate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrgutie <adrgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/15 21:11:18 by adrgutie          #+#    #+#             */
+/*   Updated: 2025/03/15 22:06:52 by adrgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+
+static char	*join_line_newline(char *lines, char *to_join)
+{
+	char	*newline;
+
+	if (lines == NULL || to_join == NULL)
+		return (free(lines), free(to_join), NULL);
+	newline = ft_strjoin(lines, to_join);
+	free(lines);
+	free(to_join);
+	return (newline);
+}
+
+char	*sandwich(char *bread, int start, int end, char *ham)
+{
+	int		len_bread;
+	char	*temp;
+	char	*bread_ham;
+
+	len_bread = ft_strlen(bread) - (end - start);
+	temp = ft_substr(bread, 0, start);
+	bread_ham = join_line_newline(temp, ham);
+	temp = ft_substr(bread, end, len_bread);
+	return (join_line_newline(bread_ham, temp));
+}
+
+char	*ft_interpolate(char *line, t_minishell *ms)
+{
+	int	single_quotes_flag;
+	int	i;
+
+	single_quotes_flag = 0;
+	i = 0;
+}
